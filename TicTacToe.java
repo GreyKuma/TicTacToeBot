09242fe187b45659cfa1d4534548520a5d4b7bb0
GreyKuma <michael-naef@hotmail.com>
2017-11-29 03:10:47 +0100

    test commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,14 +3,18 @@ package tictactoebot;
 import java.util.Scanner;
 
 public class TicTacToe {
+	// constructor
 	public TicTacToe() {
-		
+
 	}
+
+	// global variables
+	Scanner scan = new Scanner(System.in);
+	private int input = -1;
+	private String[] gamestate = {" ", " ", " ","_", "_", "_", "_", "_", "_"};
+	private boolean x = true;
+
 	public void play(){
-		Scanner scan = new Scanner(System.in);
-		int input = -1;
-		String[] gamestate = {" ", " ", " ","_", "_", "_", "_", "_", "_"};
-		boolean x = true;
 		while (true){
 			System.out.println("Press 1 to 9 on the numpad for the corresponding space");
 			input = scan.nextInt();
@@ -29,22 +33,22 @@ public class TicTacToe {
 					if (checkWinCondition(gamestate)) {
 						if(x){
 							System.out.println("X has won!\n");
-							
+
 						}else{
 							System.out.println("O has won!\n");
 						}
-						printgame(gamestate);
+						printboard();
 						break;
 					}
 					x = !x;
 				}
 			}
-			printgame(gamestate);
+			printboard();
 		}
 		scan.close();
 	}
-	
-	public static boolean checkWinCondition(String[] gs){
+
+	public boolean checkWinCondition(String[] gs){
 		boolean win = false;
 		for(int i = 0; i < 3; i++){
 			win =	win ||
@@ -52,7 +56,7 @@ public class TicTacToe {
 					((gs[i]+gs[i+3]+gs[i+6]).equals("OOO")) ||
 					((gs[3*i]+gs[3*i+1]+gs[3*i+2]).equals("XXX")) ||
 					((gs[3*i]+gs[3*i+1]+gs[3*i+2]).equals("OOO"));
-			
+
 		}
 		win = 	win ||
 				((gs[0]+gs[4]+gs[8]).equals("XXX")) ||
@@ -61,13 +65,18 @@ public class TicTacToe {
 				((gs[2]+gs[4]+gs[6]).equals("OOO"));
 		return win;
 	}
-	public static void printgame(String[] gs){
-		String spielfeld = 
-				  "_"+gs[6]+"_|_"+gs[7]+"_|_"+gs[8]+"_\n"
-				+ "_"+gs[3]+"_|_"+gs[4]+"_|_"+gs[5]+"_\n"
-				+ " "+gs[0]+" | "+gs[1]+" | "+gs[2]+" \n";
+
+	public void printboard(){
+		String[] gs = gamestate;
+		String spielfeld =
+				"_"+gs[6]+"_|_"+gs[7]+"_|_"+gs[8]+"_\n"
+						+ "_"+gs[3]+"_|_"+gs[4]+"_|_"+gs[5]+"_\n"
+						+ " "+gs[0]+" | "+gs[1]+" | "+gs[2]+" \n";
 		System.out.println(spielfeld);
 	}
-	
-	
+
+
+	public boolean makeMove() {
+		return true;
+	}
 }
